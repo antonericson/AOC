dac77f3c16206e8aee2e191db6a7d679ea05f743
Anton Ericson <antonericson97@gmail.com>
2022-12-02 16:19:32 +0100

    Updated init script

--- 2022/init.sh
+++ 2022/init.sh
@@ -2,14 +2,38 @@
 if [ ! -d "$1" ]; then
     mkdir $1
     touch $1/input.txt $1/debug.txt $1/solution.py
-    echo "def main():
-    inputLines =  open(\"debug.txt\", \"r\").readlines()
+    echo "import time
 
-    print(f'Part one: ')
-    print(f'Part two: ')
+def main():
+    with open(\"debug.txt\", \"r\") as file:
+        st1 = time.time()
+        solve_one(file)
+        et1 = time.time()
+        print(f'Part one solved in: {print_time(st1, et1)}')
+
+
+        st2 = time.time()
+        solve_two(file)
+        et2 = time.time()
+        print(f'Part one solved in: {print_time(st2, et2)}')
+
+def solve_one(file):
+    return # Return nothing, print results before this
+
+def solve_two(file):
+    return # Return nothing, print results before this
+
+
+def print_time(st, et):
+    res = et - st
+    if res < 1:
+        return f'{res * 1000} milliseconds'
+    return f'{res} seconds'
+
 
 if __name__ == \"__main__\":
-    main()" >> $1/solution.py
+    main()
+" >> $1/solution.py
     echo "$1/ created!"
 else
     echo "Files for day $1 already exist"
